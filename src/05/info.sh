4ff47df17f9e46f12deeebf5185f76ddb1ea18b5
vboxuser <vboxuser@DebOS1.myguest.virtualbox.org>
2024-01-23 21:14:30 +0700

    add

--- src/05/info.sh
+++ src/05/info.sh
@@ -3,11 +3,13 @@
 path=$1
 
 start_time=`date +%s`
-echo $path
-#1point -> Total number of folders
+
+# 1 point -> Total number of folders
 total_folders=$(find "$path" -type d | wc | awk '{print $1}')
 echo "Total number of folders (including all nested ones) = $total_folders"
 
+# 2 point -> TOP 5 folders of maximum size
+echo "TOP 5 folders of maximum size arranged in descending order (path and size):"
 
 
 
